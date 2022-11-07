693f6f18a95ef2af508e63969f33ab19e81ac083
andyschul <abschultz1@gmail.com>
2022-11-06 17:53:32 -0800

    Use astrapy sdk

--- api/api.py
+++ api/api.py
@@ -1,28 +1,38 @@
 from flask import Flask, request
-from cassandra.cluster import Cluster
 from confluent_kafka import Producer, KafkaError
 import json
 import requests
-import spotipy
-from spotipy.oauth2 import SpotifyClientCredentials
-cluster = Cluster()
-session = cluster.connect()
-import ccloud_lib
-from pathlib import Path
+from astrapy.client import create_astra_client
+import os
 
+ASTRA_DB_ID = os.environ['ASTRA_DB_ID']
+ASTRA_DB_REGION = os.environ['ASTRA_DB_REGION']
+ASTRA_DB_APPLICATION_TOKEN = os.environ['ASTRA_DB_APPLICATION_TOKEN']
+ASTRA_DB_KEYSPACE = 'music'
+ASTRA_DB_TABLE = 'albums'
+SPOTIFY_CLIENT_ID = os.environ['SPOTIFY_CLIENT_ID']
+SPOTIFY_CLIENT_SECRET = os.environ['SPOTIFY_CLIENT_SECRET']
 
-topic = 'test1'
-conf = ccloud_lib.read_ccloud_config(Path(__file__).parent / './.kafka.config')
-api_conf = ccloud_lib.read_ccloud_config(Path(__file__).parent / './.api.config')
-spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials(client_id=api_conf['spotify.client_id'], client_secret=api_conf['spotify.client_secret']))
+headers = {
+  'content-type': 'application/json',
+  'x-cassandra-token': ASTRA_DB_APPLICATION_TOKEN
+}
 
-# Create Producer instance
-producer_conf = ccloud_lib.pop_schema_registry_params_from_config(conf)
-producer = Producer(producer_conf)
+astra_client = create_astra_client(astra_database_id=ASTRA_DB_ID,
+                                   astra_database_region=ASTRA_DB_REGION,
+                                   astra_application_token=ASTRA_DB_APPLICATION_TOKEN)
 
-# Create topic if needed
-ccloud_lib.create_topic(conf, topic)
+# TODO: integrate kafka
+# topic = 'test1'
+# conf = ccloud_lib.read_ccloud_config(Path(__file__).parent / './.kafka.config')
+# api_conf = ccloud_lib.read_ccloud_config(Path(__file__).parent / './.api.config')
 
+# # Create Producer instance
+# producer_conf = ccloud_lib.pop_schema_registry_params_from_config(conf)
+# producer = Producer(producer_conf)
+
+# # Create topic if needed
+# ccloud_lib.create_topic(conf, topic)
 
 app = Flask(__name__)
 
@@ -30,34 +40,35 @@ app = Flask(__name__)
 def home():
     followers = requests.get('https://api.spotify.com/v1/me/following?type=artist', headers={'Authorization': request.headers['Authorization']})
     artist_ids = [artist['id'] for artist in followers.json()['artists']['items']]
-    producer.produce(topic, key="spotify", value=json.dumps({'artist_ids': artist_ids}))
+    # producer.produce(topic, key="spotify", value=json.dumps({'artist_ids': artist_ids}))
 
-    artist_ids = ', '.join(["'{}'".format(artist['id']) for artist in followers.json()['artists']['items']])
-    select_all = '''
-    SELECT id, name, release_date, album_type, artist_id, artist_name, image_url FROM music.albums WHERE release_date >= '2022-10-01' AND artist_id IN ({});
-    '''.format(artist_ids)
-    rows = session.execute(select_all)
+    query = {
+        'artist_id': {'$in': artist_ids},
+        'release_date': {'$gt': '2010-10-01'}
+    }
+    options = {'fields': 'id,name,release_date,album_type,artist_id,artist_name,image_url'}
+    res = astra_client.rest.search_table(keyspace=ASTRA_DB_KEYSPACE, table=ASTRA_DB_TABLE, query=query, options=options)
 
     result = {}
-    for row in rows:
-        date = row.release_date
+    for row in res['data']:
+        date = row['release_date']
         album_data = {
-            'id': row.id,
-            'name': row.name,
-            'image_url': row.image_url,
-            'release_date': row.release_date,
-            'album_type': row.album_type,
-            'artist_id': row.artist_id,
-            'artist_name': row.artist_name
+            'id': row['id'],
+            'name': row['name'],
+            'image_url': row['image_url'],
+            'release_date': row['release_date'],
+            'album_type': row['album_type'],
+            'artist_id': row['artist_id'],
+            'artist_name': row['artist_name']
         }
         if date not in result:
             result[date] = {
                 'singles': [],
                 'albums': []
             }
-        if row.album_type == 'single':
+        if row['album_type'] == 'single':
             result[date]['singles'].append(album_data)
-        elif row.album_type == 'album':
+        elif row['album_type'] == 'album':
             result[date]['albums'].append(album_data)
 
     return json.dumps(dict(sorted(result.items(), reverse=True)))
--- test-cassandra/test.py
+++ test-cassandra/test.py
@@ -1,58 +1,136 @@
-from cassandra.cluster import Cluster
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
-spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
+import requests
+import json
+from astrapy.client import create_astra_client
+import os
 
-cluster = Cluster()
-session = cluster.connect()
+SPOTIFY_CLIENT_ID = os.environ['SPOTIFY_CLIENT_ID']
+SPOTIFY_CLIENT_SECRET = os.environ['SPOTIFY_CLIENT_SECRET']
+ASTRA_DB_ID = os.environ['ASTRA_DB_ID']
+ASTRA_DB_REGION = os.environ['ASTRA_DB_REGION']
+ASTRA_DB_APPLICATION_TOKEN = os.environ['ASTRA_DB_APPLICATION_TOKEN']
+ASTRA_DB_KEYSPACE = 'music'
+ASTRA_DB_TABLE = 'albums'
 
-create_keyspace = '''
-CREATE KEYSPACE IF NOT EXISTS music
-  WITH REPLICATION = { 
-   'class' : 'SimpleStrategy', 
-   'replication_factor' : 1 
-  };
-'''
-session.execute(create_keyspace)
+spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials(client_id=SPOTIFY_CLIENT_ID, client_secret=SPOTIFY_CLIENT_SECRET))
 
-drop_table = '''
-DROP TABLE IF EXISTS music.albums
-'''
-session.execute(drop_table)
+astra_client = create_astra_client(astra_database_id=ASTRA_DB_ID,
+                                   astra_database_region=ASTRA_DB_REGION,
+                                   astra_application_token=ASTRA_DB_APPLICATION_TOKEN)
 
-create_table = '''
-CREATE TABLE IF NOT EXISTS music.albums (
-   id text, 
-   href text,
-   image_url text,
-   name text, 
-   release_date text, 
-   album_type text,
-   uri text,
-   artist_id text, 
-   artist_uri text, 
-   artist_name text, 
-   artist_href text, 
-   PRIMARY KEY (artist_id, release_date, id));
-'''
-session.execute(create_table)
+headers = {
+  'content-type': 'application/json',
+  'x-cassandra-token': ASTRA_DB_APPLICATION_TOKEN
+}
 
-# artist_id = '2WX2uTcsvV5OnS0inACecP'
-# results = spotify.artist_albums(artist_id)
-# albums = results['items']
-# while results['next']:
-#     results = spotify.next(results)
-#     albums.extend(results['items'])
+def delete_table():
+  url = 'https://{}-{}.apps.astra.datastax.com/api/rest/v2/schemas/keyspaces/{}/tables/albums'.format(ASTRA_DB_ID, ASTRA_DB_REGION, ASTRA_DB_KEYSPACE)
+  res = requests.delete(url, headers=headers)
 
-# for album in albums:
-#     if album['release_date_precision'] != 'day':
-#         continue
-#     for artist in album['artists']:
-#         insert_into = '''
-#         INSERT INTO music.albums (id, href, image_url, name, release_date, album_type, uri, artist_id, artist_uri, artist_name, artist_href) VALUES ('{}', '{}', '{}', $${}$$, '{}', '{}', '{}', '{}', '{}', '{}', '{}');
-#         '''.format(album['id'], album['href'], album['images'][0]['url'], album['name'], album['release_date'], album['album_type'], album['uri'], artist['id'], artist['uri'], artist['name'], artist['href'])
-#         session.execute(insert_into)
+def create_table():
+  url = 'https://{}-{}.apps.astra.datastax.com/api/rest/v2/schemas/keyspaces/{}/tables'.format(ASTRA_DB_ID, ASTRA_DB_REGION, ASTRA_DB_KEYSPACE)
+  data = {
+    "name": "albums",
+    "ifNotExists": True,
+    "columnDefinitions": [
+      {"name": "id", "typeDefinition": "text"},
+      {"name": "href", "typeDefinition": "text"},
+      {"name": "image_url", "typeDefinition": "text"},
+      {"name": "name", "typeDefinition": "text"},
+      {"name": "release_date", "typeDefinition": "text"},
+      {"name": "album_type", "typeDefinition": "text"},
+      {"name": "uri", "typeDefinition": "text"},
+      {"name": "artist_id", "typeDefinition": "text"},
+      {"name": "artist_uri", "typeDefinition": "text"},
+      {"name": "artist_name", "typeDefinition": "text"},
+      {"name": "artist_href", "typeDefinition": "text"}
+    ],
+    "primaryKey":
+      {
+        "partitionKey": ["artist_id"],
+        "clusteringKey": ["release_date", "id"]
+      },
+    "tableOptions": {"defaultTimeToLive":0}}
+  res = requests.post(url, headers=headers, data=json.dumps(data))
 
+def load_birdy():
+  url = 'https://{}-{}.apps.astra.datastax.com/api/rest/v2/keyspaces/music/albums'.format(ASTRA_DB_ID, ASTRA_DB_REGION)
+  artist_id = '2WX2uTcsvV5OnS0inACecP'
+  results = spotify.artist_albums(artist_id)
+  albums = results['items']
+
+  while results['next']:
+      results = spotify.next(results)
+      albums.extend(results['items'])
+
+  count = 0
+  for album in albums:
+    if count > 30:
+      break
+    count += 1
+    if album['release_date_precision'] != 'day':
+        continue
+    for artist in album['artists']:
+        data = {
+          'id': album['id'],
+          'href': album['href'],
+          'image_url': album['images'][0]['url'],
+          'name': album['name'],
+          'release_date': album['release_date'],
+          'album_type': album['album_type'],
+          'uri': album['uri'],
+          'artist_id': artist['id'],
+          'artist_uri': artist['uri'],
+          'artist_name': artist['name'],
+          'artist_href': artist['href']
+        }
+        row = astra_client.rest.add_row(keyspace=ASTRA_DB_KEYSPACE, table=ASTRA_DB_TABLE, row=data)
+
+def get_birdy():
+  query = {
+    'artist_id': {'$in': ['2WX2uTcsvV5OnS0inACecP']},
+    'release_date': {'$gt': '2010-10-01'}
+  }
+  options = {'fields': 'id,name,release_date,album_type,artist_id,artist_name,image_url'}
+  res = astra_client.rest.search_table(keyspace=ASTRA_DB_KEYSPACE, table=ASTRA_DB_TABLE, query=query, options=options)
+  print(res)
+
+get_birdy()
+
+# cluster = Cluster()
+# session = cluster.connect()
+
+# create_keyspace = '''
+# CREATE KEYSPACE IF NOT EXISTS music
+#   WITH REPLICATION = {
+#    'class' : 'SimpleStrategy',
+#    'replication_factor' : 1
+#   };
+# '''
+# session.execute(create_keyspace)
+
+# drop_table = '''
+# DROP TABLE IF EXISTS music.albums
+# '''
+# session.execute(drop_table)
+
+# create_table = '''
+# CREATE TABLE IF NOT EXISTS music.albums (
+#    id text,
+#    href text,
+#    image_url text,
+#    name text,
+#    release_date text,
+#    album_type text,
+#    uri text,
+#    artist_id text,
+#    artist_uri text,
+#    artist_name text,
+#    artist_href text,
+#    PRIMARY KEY (artist_id, release_date, id));
+# '''
+# session.execute(create_table)
 
 # results = spotify.new_releases()
 # albums = results['albums']['items']
