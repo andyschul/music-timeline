51c1bdcb756bd2128fa190aa4654cfba65cba07a
andyschul <abschultz1@gmail.com>
2022-11-12 13:40:58 -0800

    Update albums table

--- test-cassandra/test.py
+++ test-cassandra/test.py
@@ -5,6 +5,8 @@ import json
 from astrapy.client import create_astra_client
 import os
 
+DISCOGS_CONSUMER_ID = os.environ['DISCOGS_CONSUMER_ID']
+DISCOGS_CONSUMER_SECRET = os.environ['DISCOGS_CONSUMER_SECRET']
 SPOTIFY_CLIENT_ID = os.environ['SPOTIFY_CLIENT_ID']
 SPOTIFY_CLIENT_SECRET = os.environ['SPOTIFY_CLIENT_SECRET']
 ASTRA_DB_ID = os.environ['ASTRA_DB_ID']
@@ -25,13 +27,43 @@ headers = {
 }
 
 def delete_table():
-  url = 'https://{}-{}.apps.astra.datastax.com/api/rest/v2/schemas/keyspaces/{}/tables/albums'.format(ASTRA_DB_ID, ASTRA_DB_REGION, ASTRA_DB_KEYSPACE)
+  url = f'https://{ASTRA_DB_ID}-{ASTRA_DB_REGION}.apps.astra.datastax.com/api/rest/v2/schemas/keyspaces/{ASTRA_DB_KEYSPACE}/tables/{ASTRA_DB_TABLE}'
   res = requests.delete(url, headers=headers)
+  print(res)
 
 def create_table():
-  url = 'https://{}-{}.apps.astra.datastax.com/api/rest/v2/schemas/keyspaces/{}/tables'.format(ASTRA_DB_ID, ASTRA_DB_REGION, ASTRA_DB_KEYSPACE)
+  udt_url = f'https://{ASTRA_DB_ID}-{ASTRA_DB_REGION}.apps.astra.datastax.com/api/rest/v2/schemas/keyspaces/{ASTRA_DB_KEYSPACE}/types'
+  udt_data = {
+  "name": "artist",
+  "fields":[
+      {
+        "name": "id",
+        "typeDefinition": "text"
+      },
+      {
+        "name": "name",
+        "typeDefinition": "text"
+      },
+      {
+        "name": "type",
+        "typeDefinition": "text"
+      },
+      {
+        "name": "href",
+        "typeDefinition": "text"
+      },
+      {
+        "name": "uri",
+        "typeDefinition": "text"
+      }
+    ]
+  }
+  res = requests.post(udt_url, headers=headers, data=json.dumps(udt_data))
+  print(res)
+
+  url = f'https://{ASTRA_DB_ID}-{ASTRA_DB_REGION}.apps.astra.datastax.com/api/rest/v2/schemas/keyspaces/{ASTRA_DB_KEYSPACE}/tables'
   data = {
-    "name": "albums",
+    "name": ASTRA_DB_TABLE,
     "ifNotExists": True,
     "columnDefinitions": [
       {"name": "id", "typeDefinition": "text"},
@@ -40,11 +72,16 @@ def create_table():
       {"name": "name", "typeDefinition": "text"},
       {"name": "release_date", "typeDefinition": "text"},
       {"name": "album_type", "typeDefinition": "text"},
+      {"name": "album_group", "typeDefinition": "text"},
+      {"name": "album_artists", "typeDefinition": "list<artist>"},
       {"name": "uri", "typeDefinition": "text"},
+      {"name": "total_tracks", "typeDefinition": "int"},
       {"name": "artist_id", "typeDefinition": "text"},
       {"name": "artist_uri", "typeDefinition": "text"},
       {"name": "artist_name", "typeDefinition": "text"},
-      {"name": "artist_href", "typeDefinition": "text"}
+      {"name": "artist_href", "typeDefinition": "text"},
+      {"name": "artist_image", "typeDefinition": "text"},
+      {"name": "artist_genres", "typeDefinition": "set<text>"}
     ],
     "primaryKey":
       {
@@ -53,126 +90,61 @@ def create_table():
       },
     "tableOptions": {"defaultTimeToLive":0}}
   res = requests.post(url, headers=headers, data=json.dumps(data))
+  print(res)
 
-def load_birdy():
-  url = 'https://{}-{}.apps.astra.datastax.com/api/rest/v2/keyspaces/music/albums'.format(ASTRA_DB_ID, ASTRA_DB_REGION)
-  artist_id = '2WX2uTcsvV5OnS0inACecP'
-  results = spotify.artist_albums(artist_id)
+def load_artist(artist_id):
+  artist = spotify.artist(artist_id)
+  results = spotify.artist_albums(artist_id, country='US')
   albums = results['items']
 
   while results['next']:
       results = spotify.next(results)
       albums.extend(results['items'])
 
+  # TODO: Remove clean albums from results
+
+  tasks = []
   count = 0
   for album in albums:
     if count > 30:
       break
     count += 1
-    if album['release_date_precision'] != 'day':
+    if album['release_date_precision'] != 'day' or album['album_type'] == 'compilation':
         continue
-    for artist in album['artists']:
-        data = {
-          'id': album['id'],
-          'href': album['href'],
-          'image_url': album['images'][0]['url'],
-          'name': album['name'],
-          'release_date': album['release_date'],
-          'album_type': album['album_type'],
-          'uri': album['uri'],
-          'artist_id': artist['id'],
-          'artist_uri': artist['uri'],
-          'artist_name': artist['name'],
-          'artist_href': artist['href']
-        }
-        row = astra_client.rest.add_row(keyspace=ASTRA_DB_KEYSPACE, table=ASTRA_DB_TABLE, row=data)
+    data = {
+      'id': album['id'],
+      'href': album['href'],
+      'image_url': album['images'][0]['url'],
+      'name': album['name'],
+      'release_date': album['release_date'],
+      'album_type': album['album_type'],
+      'album_group': album['album_group'],
+      'album_artists': [{key : val for key, val in sub.items() if key != 'external_urls'} for sub in album['artists']],
+      'uri': album['uri'],
+      'total_tracks': album['total_tracks'],
+      'artist_id': artist['id'],
+      'artist_uri': artist['uri'],
+      'artist_name': artist['name'],
+      'artist_href': artist['href'],
+      'artist_image': artist['images'][0]['url'],
+      'artist_genres': artist['genres']
+    }
+    row = astra_client.rest.add_row(keyspace=ASTRA_DB_KEYSPACE, table=ASTRA_DB_TABLE, row=data)
+    tasks.append(data)
 
-def get_birdy():
+def get_artist(artist_id):
   query = {
-    'artist_id': {'$in': ['2WX2uTcsvV5OnS0inACecP']},
+    'artist_id': {'$in': [artist_id]},
     'release_date': {'$gt': '2010-10-01'}
   }
   options = {'fields': 'id,name,release_date,album_type,artist_id,artist_name,image_url'}
   res = astra_client.rest.search_table(keyspace=ASTRA_DB_KEYSPACE, table=ASTRA_DB_TABLE, query=query, options=options)
   print(res)
 
-get_birdy()
+# delete_table()
+# create_table()
 
-# cluster = Cluster()
-# session = cluster.connect()
-
-# create_keyspace = '''
-# CREATE KEYSPACE IF NOT EXISTS music
-#   WITH REPLICATION = {
-#    'class' : 'SimpleStrategy',
-#    'replication_factor' : 1
-#   };
-# '''
-# session.execute(create_keyspace)
-
-# drop_table = '''
-# DROP TABLE IF EXISTS music.albums
-# '''
-# session.execute(drop_table)
-
-# create_table = '''
-# CREATE TABLE IF NOT EXISTS music.albums (
-#    id text,
-#    href text,
-#    image_url text,
-#    name text,
-#    release_date text,
-#    album_type text,
-#    uri text,
-#    artist_id text,
-#    artist_uri text,
-#    artist_name text,
-#    artist_href text,
-#    PRIMARY KEY (artist_id, release_date, id));
-# '''
-# session.execute(create_table)
-
-# results = spotify.new_releases()
-# albums = results['albums']['items']
-# while results['albums']['next']:
-#     results = spotify.next(results['albums'])
-#     albums.extend(results['albums']['items'])
-
-# for album in albums:
-#     if album['release_date_precision'] != 'day':
-#         continue
-#     for artist in album['artists']:
-#         insert_into = '''
-#         INSERT INTO music.albums (id, href, image_url, name, release_date, album_type, uri, artist_id, artist_uri, artist_name, artist_href) VALUES ('{}', '{}', '{}', $${}$$, '{}', '{}', '{}', '{}', '{}', '{}', '{}');
-#         '''.format(album['id'], album['href'], album['images'][0]['url'], album['name'], album['release_date'], album['album_type'], album['uri'], artist['id'], artist['uri'], artist['name'], artist['href'])
-#         session.execute(insert_into)
-
-
-# select_all = '''
-# SELECT id, name, release_date, album_type, artist_id, artist_name, image_url FROM music.albums WHERE release_date >= '2022-10-01' AND artist_id IN ('6FBDaR13swtiWwGhX1WQsP','2avRYQUWQpIkzJOEkf0MdY','5f7VJjfbwm532GiveGC0ZK');
-# '''
-# rows = session.execute(select_all)
-
-# result = {}
-# for row in rows:
-#     date = row.release_date
-#     album_data = {
-#         'id': row.id,
-#         'name': row.name,
-#         'image_url': row.image_url,
-#         'release_date': row.release_date,
-#         'album_type': row.album_type,
-#         'artist_id': row.artist_id,
-#         'artist_name': row.artist_name
-#     }
-#     if date not in result:
-#         result[date] = {
-#             'singles': [],
-#             'albums': []
-#         }
-#     if row.album_type == 'single':
-#         result[date]['singles'].append(album_data)
-#     elif row.album_type == 'album':
-#         result[date]['albums'].append(album_data)
-
-# print(result)
+# eminem_id = '7dGJo4pcD2V6oG8kP0tJRR'
+# big_sean_id = '0c173mlxpT3dSFRgMO8XPh'
+# load_artist(eminem_id)
+# get_artist(eminem_id)
