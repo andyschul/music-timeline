be01fb3436e6285f4c24faf3045d801f9a4a8a70
andyschul <abschultz1@gmail.com>
2022-11-13 18:32:30 -0800

    Sort and group albums

--- api/api.py
+++ api/api.py
@@ -10,7 +10,8 @@ ASTRA_DB_ID = os.environ['ASTRA_DB_ID']
 ASTRA_DB_REGION = os.environ['ASTRA_DB_REGION']
 ASTRA_DB_APPLICATION_TOKEN = os.environ['ASTRA_DB_APPLICATION_TOKEN']
 ASTRA_DB_KEYSPACE = 'music'
-ASTRA_DB_TABLE = 'albums'
+ASTRA_DB_ALBUMS_TABLE = 'albums'
+ASTRA_DB_ARTISTS_TABLE = 'artists'
 
 headers = {
   'content-type': 'application/json',
@@ -41,33 +42,52 @@ def home():
 
     producer.produce(topic, key="spotify", value=json.dumps({'artist_ids': artist_ids}))
 
-    query = {
-        'artist_id': {'$in': artist_ids},
-        'release_date': {'$gt': '2000-10-01'}
+    url = f'https://{ASTRA_DB_ID}-{ASTRA_DB_REGION}.apps.astra.datastax.com/api/rest/v2/keyspaces/{ASTRA_DB_KEYSPACE}/{ASTRA_DB_ALBUMS_TABLE}'
+    params = {
+        'where': json.dumps({
+            'artist_id': {'$in': artist_ids},
+            'release_date': {'$gt': '2000-10-01'}
+        })
     }
-    options = {'page-size': 100}
-    res = astra_client.rest.search_table(keyspace=ASTRA_DB_KEYSPACE, table=ASTRA_DB_TABLE, query=query, options=options)
+    res = requests.get(url, headers=headers, params=params)
 
-    result = {}
-    for row in res['data']:
+    j_res = json.loads(res.text)
+    albums = j_res['data']
+    while 'pageState' in j_res:
+        params.update({'page-state': j_res['pageState']})
+        res = requests.get(url, headers=headers, params=params)
+        j_res = json.loads(res.text)
+        albums.extend(j_res['data'])
+
+    grouped_albums = {}
+    for row in albums:
+        album_id = row['id']
+        album_group = row['album_group']
+        if album_id not in grouped_albums:
+            grouped_albums[album_id] = {
+                'id': row['id'],
+                'name': row['name'],
+                'image_url': row['image_url'],
+                'release_date': row['release_date'],
+                'album_type': row['album_type'],
+                'artists': row['album_artists'],
+                'appearances_by': []
+            }
+        if album_group == 'appears_on':
+            grouped_albums[album_id]['appearances_by'].append(row['artist_name'])
+
+    results = {}
+    for _, row in grouped_albums.items():
         date = row['release_date']
-        album_data = {
-            'id': row['id'],
-            'name': row['name'],
-            'image_url': row['image_url'],
-            'release_date': row['release_date'],
-            'album_type': row['album_type'],
-            'artist_id': row['artist_id'],
-            'artist_name': row['artist_name']
-        }
-        if date not in result:
-            result[date] = {
+        album_type = row['album_type']
+        if date not in results:
+            results[date] = {
                 'singles': [],
                 'albums': []
             }
-        if row['album_type'] == 'single':
-            result[date]['singles'].append(album_data)
-        elif row['album_type'] == 'album':
-            result[date]['albums'].append(album_data)
+        if album_type == 'single':
+            results[date]['singles'].append(row)
+        elif album_type == 'album':
+            results[date]['albums'].append(row)
 
-    return json.dumps(dict(sorted(result.items(), reverse=True)))
+    return json.dumps(dict(sorted(results.items(), reverse=True)))
